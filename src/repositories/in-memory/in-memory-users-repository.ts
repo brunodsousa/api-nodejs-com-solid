f5c0a4d5a5e1297558f1973e6a61b4be557071e1
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2024-01-30 21:18:15 -0300

    Caso de uso de busca de academias

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -5,6 +5,20 @@ import { randomUUID } from "node:crypto";
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = [];
 
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+
   async findById(id: string): Promise<User | null> {
     const user = this.items.find((item) => item.id === id);
 
@@ -21,20 +35,6 @@ export class InMemoryUsersRepository implements UsersRepository {
     if (!user) {
       return null;
     }
-
-    return user;
-  }
-
-  async create(data: Prisma.UserCreateInput) {
-    const user = {
-      id: randomUUID(),
-      name: data.name,
-      email: data.email,
-      password_hash: data.password_hash,
-      created_at: new Date(),
-    };
-
-    this.items.push(user);
 
     return user;
   }
